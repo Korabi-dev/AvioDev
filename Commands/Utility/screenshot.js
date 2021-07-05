e9e0a510eccd360352954abd0083f297309b1184
Korabi <korabarifi53@icloud.com>
2021-07-05 17:58:11 +0200

    I made it so you cant screenshot pornhub in non nsfw channels wow

--- Commands/Utility/screenshot.js
+++ Commands/Utility/screenshot.js
@@ -1,10 +1,7 @@
 const Screenshoter = require("discord-screenshot");
-var Filter = require("bad-words")
-Filter = new Filter()
 module.exports = {
     run: async(client, message, args) => {
-        Filter.addWords("hentai", "gore", "stripper", "daddy", "cum", "sugarbaby")
-        if(Filter.isProfane(args[0]) && !message.channel.nsfw) return message.reply(client.embed("Error", "You can not screenshot nsfw sites."))
+        if( !message.channel.nsfw) return message.reply(client.embed("Error", "You can not screenshot in non-nsfw channels."))
         if(!args[0]) return message.reply(client.embed("Error", "Please provide a link"))
         const result = await Screenshoter.screenshot(args[0])
         message.reply(result)
