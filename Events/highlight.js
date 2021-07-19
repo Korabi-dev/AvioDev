7e7d1826a74d5033cc5a174720c76d641023575b
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2021-07-19 21:43:34 +0200

    Made it so bots cant trigger highlights

--- Events/highlight.js
+++ Events/highlight.js
@@ -2,6 +2,7 @@ module.exports = {
     name: "message",
     run: async(message, client) => {
         if(message.isOwner == false && client.user.id == "855057364032684092") return;
+        if(message.author.bot) return;
         const docs = await client.models.highlights.find({})
         docs.forEach(doc => {
             if(message.content.toLowerCase().includes(doc.name)){
