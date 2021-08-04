c1f5ff55603e59f2de621367aa32f3335dcaf390
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2021-08-04 12:42:41 +0200

    Create update.js

---
+++ Commands/Owner/update.js
@@ -0,0 +1,18 @@
+module.exports = {
+    owner: true,
+    run: async(client, message, args) => {
+        const channel_id = "741222883522773023"
+        const channel = client.channels.cache.get(channel_id)
+        if(!channel) return message.reply(client.embed("Error","Could not find required channel with id: " + channel_id))
+        if(!args[0]) return message.reply(client.embed("Error", `Invalid syntax, valid syntax: \`${message.guild.prefix}update <Description> | [Color] | [Title] | [Footer]\``))
+        const input = args.all.split("|")
+        const embed = client.embed("Update" , "Avio Is being updated").setFooter(`Update by ${message.author.tag}`)
+        if(input[0]) embed.setDescription(input[0].trim())
+        if(input[1]) embed.setColor(input[1].trim().toUpperCase())
+        if(input[2]) embed.setTitle(input[2].trim())
+        if(input[3]) embed.setFooter(input[3].trim())
+        message.react("✅")
+        message.delete()
+        channel.send("@everyone,", embed)
+    }
+}
