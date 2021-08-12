61252461a05261cbd4356cbbf118d5715544bd62
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2021-08-12 18:43:36 +0200

    Fixed obfuscate command error.

--- Commands/Utility/obfuscate.js
+++ Commands/Utility/obfuscate.js
@@ -3,7 +3,14 @@ const { MessageAttachment } = require("discord.js")
 module.exports = {
     run: async(client, message, args) => {
         if(!args[0]) return message.reply(client.embed("Error", "You need to provide some javascript code to obfuscate."))
-        const obv = await o.obfuscate(args.all)
+        var obv = {}
+        obv._obfuscatedCode = "Error"
+        try{
+        obv = await o.obfuscate(args.all)
+        } catch(e){
+            obv = {}
+            obv._obfuscatedCode = `Error: ${e}`
+        }
         if(obv._obfuscatedCode.length > 4000){
             const code = new MessageAttachment(Buffer.from(obv._obfuscatedCode, "utf-8")).setName("Obfuscated Output.txt")
             return message.reply(code)
