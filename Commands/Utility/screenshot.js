b287e4a77816200c9582dd8597f180a11148c52c
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2021-08-22 13:25:28 +0200

    Removed stupid nsfw thing

--- Commands/Utility/screenshot.js
+++ Commands/Utility/screenshot.js
@@ -1,7 +1,6 @@
 const Screenshoter = require("discord-screenshot");
 module.exports = {
     run: async(client, message, args) => {
-        if( !message.channel.nsfw) return message.reply(client.embed("Error", "You can not screenshot in non-nsfw channels."))
         if(!args[0]) return message.reply(client.embed("Error", "Please provide a link"))
         const result = await Screenshoter.screenshot(args[0])
         message.reply(result)
