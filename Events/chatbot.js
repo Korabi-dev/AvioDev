3afbced0188d8e1811dac1e70b84c5711c966455
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2021-08-24 23:18:13 +0200

    Chatbot fix

--- Events/chatbot.js
+++ Events/chatbot.js
@@ -7,7 +7,7 @@ module.exports = {
         if(data.enabled == true && data.channel == message.channel.id){
             var reply = {message: "beep boop beep boop *robot go stupid*"}
             try{
-                const { data } = await axios.get(`https://api.affiliateplus.xyz/api/chatbot?message=${message.content}&botname=${client.user.username}&ownername=Korabi&user=${message.author.id}`)
+                const { data } = await axios.get(`https://api.affiliateplus.xyz/api/chatbot?message=${message.content.trim()}&botname=${client.user.username}&ownername=Korabi&user=${message.author.id}`)
                 reply = data
             }catch(e){
                 console.log(e)
