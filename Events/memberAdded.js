8ae06620676ec328f987b847bafbe75019dcac6f
Korab Arifi <korabarifi53@gmail.com>
2021-09-23 22:05:09 +0200

    Anti join fix

---
+++ Events/memberAdded.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "guildMemberAdded",
+    run: async(member, client) => {
+        const d = await client.models.automod.findOne({guild: member.guild.id})
+        if(d){
+            if(d.features.includes("antijoin")){
+                await member.send(client.embed("Kicked", `You can not join ${member.guild.name} because anti join is enabled, please try again later.`)).catch(e => e)
+               await member.kick().catch(e => e)
+            }
+        }
+    }
+}
